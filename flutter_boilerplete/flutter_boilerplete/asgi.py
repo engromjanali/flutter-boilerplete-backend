c100ebc9218cc85d4f6f7b7c346cc22c4126ecbf
romjanali <romjan.6amtech@gmail.com>
2026-09-02 20:00:32 +0600

    init

---
+++ flutter_boilerplete/flutter_boilerplete/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for flutter_boilerplete project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flutter_boilerplete.settings')
+
+application = get_asgi_application()
